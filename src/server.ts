4fd55ad9698ae182e55c5aa4a74f40f4699e61c9
mapatag <64618397+mapatag@users.noreply.github.com>
2020-04-30 23:08:10 -0500

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 
 
-import * as express from 'express';
+import express from 'express';
 import { SERVER_PORT } from './environment';
 import  * as socketIO from 'socket.io';
 import * as http from 'http';
