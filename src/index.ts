e12ea5b27a606e6962c06bb821b86c9bdf1d15b6
mapatag <64618397+mapatag@users.noreply.github.com>
2020-04-30 22:52:52 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
 
+import Server from './server';
+import router from './router';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+
+
+const server = Server.instance;
+
+// BodyParser
+server.app.use( bodyParser.urlencoded({ extended: true }) );
+server.app.use( bodyParser.json() );
+
+// CORS
+server.app.use( cors({ origin: true, credentials: true  }) );
+
+
+// Rutas de servicios
+server.app.use('/', router );
+
+
+
+
+server.start( () => {
+    console.log(`Servidor corriendo en el puerto ${ server.port }`);
+});
