70148518b9badca7ff6479e69a780276d1b0f590
mapatag <64618397+mapatag@users.noreply.github.com>
2020-05-04 14:54:44 -0500

    Update socket.ts

--- src/socket.ts
+++ src/socket.ts
@@ -3,25 +3,25 @@ import * as socketIO from 'socket.io';
 import { UsuariosLista } from '../usuarios-lista'; // -> nuevo para basico-v3
 import { Usuario } from '../usuario';              // -> nuevo para basico-v3 
 
-export const desconectar = ( cliente: Socket ) => {
+export const usuariosConectados = new UsuariosLista(); // -> nuevo para basico-v3
 
+export const conectarCliente = ( cliente: Socket ) => { // -> nuevo para basico-v3
+    const usuario = new Usuario( cliente.id );
+    usuariosConectados.agregar( usuario );
+}
+
+export const desconectar = ( cliente: Socket ) => {
     cliente.on('disconnect', () => {
         console.log('Cliente desconectado');
     });
-
 }
 
 // Escuchar mensajes
 export const mensaje = ( cliente: Socket, io: socketIO.Server ) => {
-
     cliente.on('mensaje', (  payload: { de: string, cuerpo: string }  ) => {
-
         console.log('Mensaje recibido', payload );
-
         io.emit('mensaje-nuevo', payload );
-
     });
-
 }
 
 // Configurar usuario -> nuevo para basico-v3
