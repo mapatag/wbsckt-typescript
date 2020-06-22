9ce48bf5331fedfd553d55df277d88f728ff9c11
mapatag <64618397+mapatag@users.noreply.github.com>
2020-06-22 01:28:45 -0500

    Create block.ts

---
+++ src/block.ts
@@ -0,0 +1,29 @@
+export class Block {
+
+    public txn          : string;
+    public issuer       : string;
+    public owner        : string; 
+    public price        : string;
+    public owner        : string;
+    public face_value   : string;
+    public price        : string;
+    public current_state: string;
+    public previousHash : string;
+    public hash         : string;
+
+    constructor( txn: string ) { 
+        
+      this.txn          : txn;
+      this.issuer       : issuer;
+      this.owner        : owner; 
+      this.price        : price;
+      this.owner        : owner;
+      this.face_value   : face_value;
+      this.price        : price;
+      this.current_state: current_state;
+      this.previousHash : previousHash;
+      this.hash         : hash;
+      
+    }
+
+}
