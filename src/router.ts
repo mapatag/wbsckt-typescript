f007b174c92a4257f22fb201448896627942a5f2
mapatag <64618397+mapatag@users.noreply.github.com>
2020-10-31 01:58:46 -0500

    Update router.ts

--- src/router.ts
+++ src/router.ts
@@ -41,8 +41,9 @@ router.post('/mensajes', ( req: Request, res: Response  ) => {
 
     const cuerpo = req.body.cuerpo;
     const de     = req.body.de;
+     const sala     = req.body.sala;
 
-    const payload = { cuerpo, de };  // nuevo para basico-v3
+    const payload = { cuerpo, de, sala };  // nuevo para basico-v3
 
     const server = Server.instance;  // nuevo para basico-v3
     server.io.emit('mensaje-nuevo', payload );  // nuevo para basico-v3
@@ -50,7 +51,8 @@ router.post('/mensajes', ( req: Request, res: Response  ) => {
     res.json({
         ok: true,
         cuerpo,
-        de
+        de, 
+        sala
     });
 
 });
